fbcf1ebe921465e1a4d3b1364fa09dc7fa3b533c
Clayton Parker <clayton@sixfeetup.com>
2010-12-28 10:14:48 -0500

    PEP8 for the tests too

--- src/lovely/buildouthttp/tests.py
+++ src/lovely/buildouthttp/tests.py
@@ -1,10 +1,13 @@
-import doctest, unittest
+import doctest
+import unittest
 from zope.testing.doctest import DocFileSuite, DocTestSuite
 import tempfile
 import shutil
 
+
 def setUp(test):
     test.globs['tmp'] = tempfile.mkdtemp()
+
 
 def tearDown(test):
     shutil.rmtree(test.globs['tmp'])
@@ -12,14 +15,16 @@ def tearDown(test):
 
 def test_suite():
     uSuites = (
-        DocFileSuite('install.txt',
-                     setUp=setUp, tearDown=tearDown,
-                     optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
-                     ),
-        DocTestSuite('lovely.buildouthttp.buildouthttp',
-                     setUp=setUp, tearDown=tearDown,
-                     optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
-                     ),
+        DocFileSuite(
+            'install.txt',
+            setUp=setUp,
+            tearDown=tearDown,
+            optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS,
+            ),
+        DocTestSuite(
+            'lovely.buildouthttp.buildouthttp',
+            setUp=setUp, tearDown=tearDown,
+            optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS,
+            ),
         )
     return unittest.TestSuite(uSuites)
-
