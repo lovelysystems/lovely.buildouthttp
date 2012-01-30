ee39271f783aabeef8f23631f37b455965c1e739
Alex Holmes <alex@alex-holmes.com>
2012-01-30 10:20:25 +0000

    Prepare for release 0.4.1-isotoma6

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description=(
 name='lovely.buildouthttp'
 setup(
     name = name,
-    version = "0.4.1-isotoma5",
+    version = "0.4.1-isotoma6",
     author = "Lovely Systems GmbH",
     author_email = "office@lovelysystems.com",
     description = "Buildout extension to add http authentication "
